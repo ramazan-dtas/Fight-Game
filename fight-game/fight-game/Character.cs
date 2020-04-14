43f4cd33f5f85f0c85f03063006513b165d765f0
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2020-04-14 09:53:58 +0100

    some changes

--- fight-game/fight-game/Character.cs
+++ fight-game/fight-game/Character.cs
@@ -13,39 +13,37 @@ namespace fight_game
             int hp = 100;
             int MaxHit = 20;
 
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + MaxHit);
+            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + MaxHit + "\n");
+
+
         }
 
         public void ElfLord()
         {
             int hp = 100;
             int str = 10;
-            double suceed = 0.50;
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\nattack hit succeed: " + suceed);
+            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\n");
         }
 
         public void HumanLord()
         {
             int hp = 100;
             int str = 8;
-            double suceed = 0.93;
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\nattack hit succeed: " + suceed);
+            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\n");
         }
 
         public void DogLord()
         {
             int hp = 100;
             int str = 11;
-            double suceed = 0.47;
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\nattack hit succeed: " + suceed);
+            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\n");
         }
 
         public void CatLord()
         {
             int hp = 100;
             int str = 6;
-            double suceed = 1.00;
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\nattack hit succeed: " + suceed);
+            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\n");
         }
     }
 }
--- fight-game/fight-game/Program.cs
+++ fight-game/fight-game/Program.cs
@@ -36,14 +36,30 @@ namespace fight_game
 
             if(answer == "2 Player")
             {
+                Console.WriteLine("Player 1: ");
                 string character = Functionality.Select(new string[] { "Orc Lord", "Elf Lord", "Human Lord", "Dog Lord", "Cat Lord" });
-
                 if (character == "Orc Lord") { n.OrcLord(); }
                 if (character == "Elf Lord") { n.ElfLord(); }
                 if (character == "Human Lord") { n.HumanLord(); }
                 if (character == "Dog Lord") { n.DogLord(); }
                 if (character == "Cat Lord") { n.CatLord(); }
+                
+                Console.WriteLine("\nPlayer 2: ");
+                string character1 = Functionality.Select(new string[] { "Orc Lord", "Elf Lord", "Human Lord", "Dog Lord", "Cat Lord" });
+
+                if (character1 == "Orc Lord") { n.OrcLord(); }
+                if (character1 == "Elf Lord") { n.ElfLord(); }
+                if (character1 == "Human Lord") { n.HumanLord(); }
+                if (character1 == "Dog Lord") { n.DogLord(); }
+                if (character1 == "Cat Lord") { n.CatLord(); }
+
+                Console.ReadKey();
+                Console.Clear();
+
+                
             }
+
+
             Console.ReadLine();
         }
     }
