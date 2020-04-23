b62c403d9aa57dded829dbd7525cf49f044731b6
ramazan-dtas <60772908+ramazan-dtas@users.noreply.github.com>
2020-04-23 11:51:07 +0100

    Players can fight

--- fight-game/fight-game/Character.cs
+++ fight-game/fight-game/Character.cs
@@ -5,46 +5,92 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace fight_game
-{
+{//Character class
+
     class Character
     {
-        
+        public string Name { get; set; } = "Warrior";
+        public double Hp { get; set; } = 100;
+        public double AttkMax { get; set; } = 15;
+        public double BlockMax { get; set; } = 10;
+        //Specs about OrcLord character
+
+
         public void OrcLord()
         {
-            int hp = 100;
-            int MaxHit = 20;
-
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + MaxHit + "\n");
-
-
+            Console.WriteLine("\nhp: " + Hp + "\nstrenght: " + AttkMax + "\n");
         }
 
+        //specs about other characters
         public void ElfLord()
         {
-            int hp = 100;
-            int str = 10;
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\n");
+            Console.WriteLine("\nhp: " + Hp + "\nstrenght: " + AttkMax + "\n");
         }
 
         public void HumanLord()
         {
-            int hp = 100;
-            int str = 8;
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\n");
+            Console.WriteLine("\nhp: " + Hp + "\nstrenght: " + AttkMax + "\n");
         }
 
         public void DogLord()
         {
-            int hp = 100;
-            int str = 11;
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\n");
+            Console.WriteLine("\nhp: " + Hp + "\nstrenght: " + AttkMax + "\n");
         }
 
         public void CatLord()
         {
-            int hp = 100;
-            int str = 6;
-            Console.WriteLine("\nhp: " + hp + "\nstrenght: " + str + "\n");
+            Console.WriteLine("\nhp: " + Hp + "\nstrenght: " + AttkMax + "\n");
+        }
+        /*
+        public void Fight(String f1, String f2)
+        {
+            Console.WriteLine(f1 + " " + f2);
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write(f1);
+            
+            Console.SetCursorPosition(100, 0);
+            Console.Write(f2);
+
+            var elfLord = new Stats { Name = "Elf Lord", MaxHp = 100, Strength = 10 };
+            var dogLord = new Stats { Name = "Dog Lord", MaxHp = 50, Strength = 4 };
+
+        }
+        
+        public Warrior(string name = "Warrior",
+            double health = 0,
+            double attkMax = 0,
+            double blockMax = 0)
+        {
+            
+            Hp = health;
+            AttkMax = attkMax;
+            BlockMax = blockMax;
+        }*/
+
+        Random rnd = new Random();
+
+        public Character(string name = "Warrior",
+            double health = 100,
+            double attkMax = 15,
+            double blockMax = 10)
+        {
+            Name = name;
+            Hp = health;
+            AttkMax = attkMax;
+            BlockMax = blockMax;
+        }
+
+        public double Attack()
+        {
+            return rnd.Next(1, (int)AttkMax);
+        }
+
+        public double Block()
+        {
+            return rnd.Next(1, (int)BlockMax);
         }
     }
 }
--- fight-game/fight-game/Program.cs
+++ fight-game/fight-game/Program.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace fight_game
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            
             Character n = new Character();
+            
             Console.WriteLine("Welcome to Fight Game\n\n");
             //User starts with choose if he/she wants to play 1 or 2 player
             string answer = Functionality.Select(new string[] { "1 Player", "2 Player" });
@@ -39,21 +42,28 @@ namespace fight_game
                         n.CatLord();
                         break;
                 }
-                //Start of programs character it randomize it
+                //Start of random character for robot
                 Random rnd = new Random();
 
                 string[] BotChar = { "Orc Lord", "Elf Lord", "Human Lord", "Dog Lord", "Cat Lord" };
-
                 int BotIndex = rnd.Next(BotChar.Length);
                 Console.WriteLine("Robot character: {0}", BotChar[BotIndex]);
-                
+                Console.ReadKey();
+                Console.Clear();
+
+                Character war1 = new Character(character, n.Hp, n.AttkMax, n.BlockMax);
+                Character war2 = new Character(BotChar[BotIndex], n.Hp, n.AttkMax, n.BlockMax);
+                Battle.StartFight(war1, war2);
             }
+            
             //if user choosed 2 player that means it is 2 humans play against each other
             if(answer == "2 Player")
             {
+
                 Console.WriteLine("Player 1: ");
+                //This is code so user can choose different characters with arrow keys
                 string character = Functionality.Select(new string[] { "Orc Lord", "Elf Lord", "Human Lord", "Dog Lord", "Cat Lord" });
-                
+                //switch case about diiferent characters
                 switch(character)
                 {
                     case "Orc Lord":
@@ -75,7 +85,7 @@ namespace fight_game
                 
                 Console.WriteLine("\nPlayer 2: ");
                 string character1 = Functionality.Select(new string[] { "Orc Lord", "Elf Lord", "Human Lord", "Dog Lord", "Cat Lord" });
-
+                
                 switch (character1)
                 {
                     case "Orc Lord":
@@ -96,9 +106,18 @@ namespace fight_game
                 }
                 Console.ReadKey();
                 Console.Clear();
+                Character war1 = new Character(character, n.Hp, n.AttkMax, n.BlockMax);
+                Character war2 = new Character(character1, n.Hp, n.AttkMax, n.BlockMax);
+                Battle.StartFight(war1, war2);
+
 
             }
             Console.ReadLine();
+
+    
+            Console.ReadLine();
+
         }
+        
     }
 }
